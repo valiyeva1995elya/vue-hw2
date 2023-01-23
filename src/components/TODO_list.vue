f08a5c6f64a76651e5be9c7e9b275b2ec3e48fb7
ValiyevaElya <valiyeva1995elya>
2023-01-23 16:51:17 +0600

    file

--- src/components/TODO_list.vue
+++ src/components/TODO_list.vue
@@ -1,12 +1,12 @@
 <script>
 
-
 export default {
     data() {
         return {
             tasks: [
                 {
-                    name: 'clean', 
+                    name: 'clean',
+                    
                 }
             ],
             name: " ",
@@ -20,13 +20,13 @@ export default {
             }
             this.tasks.push(newTask)
             this.name = "";
-            
+
         },
-        deleteTask(this.name){
-            tasks.filter(item => item == name)
-            console.log("ok")
+
+        deleteTask(name) {
+            this.tasks = this.tasks.filter(task => task.name == this.name)
         }
-       
+
     }
 }
 </script>
@@ -36,10 +36,7 @@ export default {
         <p> {{ counter }} task(s) left!</p>
         <div class="add_task">
             <form @submit.prevent>
-                <input v-bind:value="name" 
-                    @input="name = $event.target.value" 
-                    type="text" 
-                    class="input"
+                <input v-bind:value="name" @input="name = $event.target.value" type="text" class="input"
                     placeholder="Add a new task">
                 <button @click="addTask" class="btn">
                     Add
@@ -49,31 +46,33 @@ export default {
         <div class="task" v-for="task in tasks">
             <input type="checkbox">
             <div>{{ task.name }}</div>
-            <button class="btn2" @click="deleteTask">X</button>
+            <button class="btn2" @click="deleteTask(name)">X</button>
         </div>
     </div>
 </template>
 
 <style scoped>
-    .task{
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
-        width: 97%;
-        height: 20px;
-        border: 1px solid rgb(180, 179, 179);
-        padding: 5px 15px;
-        
-    }
-    .block-compl{
-        margin: 25px;
-    }
-    .input{
-        width: 95%;
-        background-color: black;
-        border: 1px solid rgb(180, 179, 179);
-        margin: 0 10px 20px 0;
-        border-radius: 3px;
-        color: white;
-    }
+.task {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 97%;
+    height: 20px;
+    border: 1px solid rgb(180, 179, 179);
+    padding: 5px 15px;
+
+}
+
+.block-compl {
+    margin: 25px;
+}
+
+.input {
+    width: 95%;
+    background-color: black;
+    border: 1px solid rgb(180, 179, 179);
+    margin: 0 10px 20px 0;
+    border-radius: 3px;
+    color: white;
+}
 </style>
